235ab45535596cdeea63da9be0217efe7620493b
MAHAM42 <161121868+MAHAM42@users.noreply.github.com>
2024-04-24 11:15:19 -0700

    9th-Assignment
    TypeScript 45-Assignment of Q9 with Node.js

---
+++ main9.js
@@ -0,0 +1,10 @@
+"use strict";
+//ASSIGNMENT NO 09:
+Object.defineProperty(exports, "__esModule", { value: true });
+/*
+  Favorite Number: Store your favorite number in a variable.
+  Then, using that variable, create a message that reveals your favorite number. Print that message.
+*/
+console.log("ASSIGNMENT NO 09:");
+let favouriteNumber = 9;
+console.log(`My favouite number is ${favouriteNumber}.`);
---
+++ main9.ts
@@ -0,0 +1,11 @@
+//ASSIGNMENT NO 09:
+
+/*
+  Favorite Number: Store your favorite number in a variable. 
+  Then, using that variable, create a message that reveals your favorite number. Print that message.
+*/
+
+console.log("ASSIGNMENT NO 09:");
+
+let favouriteNumber: number = 9;
+console.log(`My favouite number is ${favouriteNumber}.`);
